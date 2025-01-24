5bbeb9378e3ecabf076ef08cfaeea93a7e58c160
Philip Fackler <facklerpw@ornl.gov>
2025-01-24 11:21:05 -0600

    Add CI for cpus

--- test/test_calculateIntersections.jl
+++ test/test_calculateIntersections.jl
@@ -14,13 +14,14 @@ import JACC
     histogram = Hist3(x, y, z)
 
     doctest = MiniVATES.MDNorm(x, y, z)
-    maxIx = MiniVATES.maxIntersections(doctest)
+    maxIx = MiniVATES.maxIntersections(histogram)
 
     open(calc_intersections_file) do f
         line = split(strip(readline(f)), ' ')
         transform = transpose(SquareMatrix3c(parse.(ScalarType, line)))
 
         ndets = parse(Int, readline(f))
+        doctest.intersections = MiniVATES.PreallocJaggedArray{Crd4}(ndets, maxIx)
 
         for i = 1:ndets
             line = split(strip(readline(f)), ' ')
@@ -39,20 +40,30 @@ import JACC
                 push!(values, parse.(ScalarType, line))
             end
 
-            intersections = MiniVATES.row(doctest.intersections, 1)
-            MiniVATES.calculateIntersections!(
-                histogram.edges[1],
-                histogram.edges[2],
-                histogram.edges[3],
-                histogram,
-                theta,
-                phi,
-                transform,
-                lowvalue,
-                highvalue,
-                intersections,
-                # iPerm,
+            JACC.parallel_for(1,
+                (i, t) -> begin
+                    intersections = MiniVATES.row(t.intersections, 1)
+                    MiniVATES.calculateIntersections!(
+                        t.histogram,
+                        t.theta,
+                        t.phi,
+                        t.transform,
+                        t.lowvalue,
+                        t.highvalue,
+                        intersections,
+                    )
+                end,
+                (
+                    intersections = doctest.intersections,
+                    histogram,
+                    theta,
+                    phi,
+                    transform,
+                    lowvalue,
+                    highvalue
+                ),
             )
+            intersections = MiniVATES.row(doctest.intersections, 1)
             @test length(intersections) == num_intersections
             for i = 1:num_intersections
                 for j = 1:4
