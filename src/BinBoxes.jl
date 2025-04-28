27bbdee7db0d6258e96b15851e250b66aceb21ae
Steven Hahn <hahnse@ornl.gov>
2025-04-28 16:21:55 -0400

    set threshold
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- src/BinBoxes.jl
+++ src/BinBoxes.jl
@@ -1,10 +1,10 @@
 import StaticArrays: SMatrix, MVector
 import Atomix
 
-function binMD!(h::Hist3, events::AbstractArray, weights::AbstractArray, op)
+function binMD!(h::Hist3, events::AbstractArray, weights::AbstractArray, op::SquareMatrix3c)
     for n = 1:size(events, 1)
         v = op * C3[events[n, 1], events[n, 2], events[n, 3]]
-    atomic_push!(h, v[1], v[2], v[3], weights[n])
+        atomic_push!(h, v[1], v[2], v[3], weights[n])
     end
 end
 
@@ -15,43 +15,57 @@ function binBoxes!(h::Hist3, events::FastEventData, transforms::AbstractArray)
             @inbounds begin
                 eid = t.eventIndex
                 if t.boxType[i] == 1 && eid[i, 2] != 0
-                    vi = SMatrix{3,8,CoordType}(t.extents[i, 1], t.extents[i, 3], t.extents[i, 5],
-                                                t.extents[i, 1], t.extents[i, 3], t.extents[i, 6],
-                                                t.extents[i, 1], t.extents[i, 4], t.extents[i, 5],
-                                                t.extents[i, 1], t.extents[i, 4], t.extents[i, 6],
-                                                t.extents[i, 2], t.extents[i, 3], t.extents[i, 5],
-                                                t.extents[i, 2], t.extents[i, 3], t.extents[i, 6],
-                                                t.extents[i, 2], t.extents[i, 4], t.extents[i, 5],
-                                                t.extents[i, 2], t.extents[i, 4], t.extents[i, 6])
-                    for op in t.transforms
-                        vf = op * vi
-                        startIdx = MVector{3,SizeType}(0, 0, 0)
-                        singleBox = true
-                        for j = 1:3
-                            startIdx[j] = binindex1d(t.h, j, vf[j, 1])
-                            if startIdx[j] < 1 || startIdx[j] > t.h.nbins[j]
-                                singleBox = false
-                                break
-                            end
-                            for k = 2:8
-                                endIdx = binindex1d(t.h, j, vf[j, k])
-                                if startIdx[j] != endIdx
+                    if (eid[i, 2]) > 16
+                        vi = SMatrix{3,8,CoordType}(t.extents[i, 1], t.extents[i, 3], t.extents[i, 5],
+                                                    t.extents[i, 1], t.extents[i, 3], t.extents[i, 6],
+                                                    t.extents[i, 1], t.extents[i, 4], t.extents[i, 5],
+                                                    t.extents[i, 1], t.extents[i, 4], t.extents[i, 6],
+                                                    t.extents[i, 2], t.extents[i, 3], t.extents[i, 5],
+                                                    t.extents[i, 2], t.extents[i, 3], t.extents[i, 6],
+                                                    t.extents[i, 2], t.extents[i, 4], t.extents[i, 5],
+                                                    t.extents[i, 2], t.extents[i, 4], t.extents[i, 6])
+                        for op in t.transforms
+                            vf = op * vi
+                            startIdx = MVector{3,SizeType}(0, 0, 0)
+                            singleBox = true
+                            for j = 1:3
+                                startIdx[j] = binindex1d(t.h, j, vf[j, 1])
+                                if startIdx[j] < 1 || startIdx[j] > t.h.nbins[j]
                                     singleBox = false
                                     break
                                 end
+                                for k = 2:8
+                                    endIdx = binindex1d(t.h, j, vf[j, k])
+                                    if startIdx[j] != endIdx
+                                        singleBox = false
+                                        break
+                                    end
+                                end
+                                if singleBox == false
+                                    break
+                                end
                             end
-                            if singleBox == false
-                                break
+                            if singleBox
+                                Atomix.@atomic binweights(t.h)[startIdx...] += t.boxSignal[i, 1]
+                            else
+                                startId = eid[i, 1] + 1
+                                stopId = startId + eid[i, 2] - 1
+                                localevents = @view t.events[startId:stopId, :]
+                                localweights = @view t.weights[startId:stopId]
+                                binMD!(t.h, localevents, localweights, op)
                             end
                         end
-                        if singleBox
-                            Atomix.@atomic binweights(t.h)[startIdx...] += t.boxSignal[i, 1]
-                        else
-                            startId = eid[i, 1] + 1
-                            stopId = startId + eid[i, 2] - 1
-                            localevents = @view t.events[startId:stopId, :]
-                            localweights = @view t.weights[startId:stopId]
-                            binMD!(t.h, localevents, localweights, op)
+                    else
+                        startId = eid[i, 1] + 1
+                        stopId = startId + eid[i, 2] - 1
+                        localevents = @view t.events[startId:stopId, :]
+                        localweights = @view t.weights[startId:stopId]
+                        for n = 1:size(localevents, 1)
+                            col = C3[localevents[n, 1], localevents[n, 2], localevents[n, 3]]
+                            for op in t.transforms
+                                v = op * col
+                                atomic_push!(h, v[1], v[2], v[3], localweights[n])
+                            end
                         end
                     end
                 end
